a515458b513575fa93a184fc671f9b68e1baa903
juancgarcia <juangarcia.info@gmail.com>
2013-03-18 22:31:07 -0400

    Tidying up

--- scripts/test-@linHome.sh
+++ scripts/test-@linHome.sh
@@ -6,6 +6,6 @@ echo ""
 echo "Starting Testacular Server (http://vojtajina.github.com/testacular)"
 echo "-------------------------------------------------------------------"
 
-export PHANTOMJS_BIN=~/Apps/phantomjs-1.8.2-linux-x86_64/phantomjs
+export PHANTOMJS_BIN=/home/ziekrage/Apps/phantomjs-1.8.2-linux-x86_64/bin/phantomjs
 
 testacular start $BASE_DIR/../config/testacular.conf.js $*
