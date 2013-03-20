f825ff79f38d85a87cbf45fde0563266e08c2efa
juancgarcia <jgarcia@W7-1350.padomain.com>
2013-03-20 17:04:04 -0400

    added date picker

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,9 @@ var ieFixes = angular.module('ieFixes', []);
 
 
 // Declare app level module which depends on filters, and services
-window.myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ieFixes']).
+window.myApp = angular.module('myApp', [
+	'myApp.filters', 'myApp.services', 'myApp.directives', 'ieFixes', 'ui.date'
+	]).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/contracts/wizard', {templateUrl: 'partials/contract-wizard.html', controller: 'ContractWizardCtrl'});
   }]);
