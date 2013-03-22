cbed21c8bd5f67ceb5cf29179c829f391d4d0aa1
juancgarcia <juangarcia.info@gmail.com>
2013-03-22 12:55:14 -0400

    fixed stateMachine, renamed to linearStateMachine

--- app/js/helpers.js
+++ app/js/helpers.js
@@ -14,68 +14,125 @@ myApp.helper.classes = {};
 myApp.helper.classes.state = (function() {
 	var stateClass = function(config){
 		config = config || {};
+		this.dataBlob = null;
 		this.name = config.name || guid();
-	};
+		this.init(config);
+	};	
+	stateClass.prototype.init = function(_config){
+		this.dataBlob = _config;
+	}
 	stateClass.prototype.name = '--unnamed--';
 	stateClass.prototype.getName = function(){
 		return this.name;
+	};
+	stateClass.prototype.getData = function(){
+		return this.dataBlob;
 	};
 
 	return stateClass;
 })();
 
 myApp.helper.classes.stateList = (function(){
-	var nameToIndex, indexToName, nextInsert, dataStructure;
 
 	var stateListClass = function(config){
-		nameToIndex = {};
-		indexToName = {};
-		nextInsert = 0;
+		this.nameToIndex = {};
+		this.indexToName = {};
+		this.dataStructure = {};
+		this.nextInsert = 0;
+
+		if(config)
+			this.init(config);
+	};
+	stateListClass.prototype.init = function(_config){
+		var statesList;
+		if(!this.isEmpty() || _config === undefined)
+			return;
+
+		if( _config instanceof Array ){
+			statesList = _config;
+		} else if( _config.states !== undefined ){
+			statesList = _config.states;
+		} else
+			statesList = [];
+
+		this.add(statesList);
 	};
 	stateListClass.prototype.add = function(state_s){
 		if(Array.isArray(state_s))
 			addMany.call(this, state_s);
 		else
 			addMany.call(this, [state_s]);
+	};
+	function addOne(state){
+		if( !(state instanceof myApp.helper.classes.state) )
+			state = new myApp.helper.classes.state(state);
 
-		function addOne(state){
-			if( !(state instanceof myApp.helper.classes.stateList) )
-				state = new myApp.helper.classes.stateList(state);
-
-			nameToIndex[state.getName()] = nextInsert;
-			indexToName[nextInsert] = state.getName();
-			nextInsert++;
+		this.nameToIndex[state.getName()] = this.nextInsert;
+		this.indexToName[this.nextInsert] = state.getName();
+		this.dataStructure[state.getName()] = state;
+		this.nextInsert++;
+	}
+	function addMany(states){
+		for(var i=0; i<states.length; i++){
+			addOne.call(this, states[i]);
 		}
-		function addMany(states){}
+	}
+	getIndex = function(name){
+		var index;
+		if( (index = this.nameToIndex[name]) == undefined)
+			throw "noSuchState";
+		return index;
 	};
+	/*return state obj*/
 	stateListClass.prototype.get = function(name){
-		var foo;
-		if(false)
-			foo = 12;
+		var index;
+		if( (index = this.nameToIndex[name]) == undefined)
+			throw "noSuchState";
+		return this.dataStructure[name];
 	};
+	/*return state obj*/
+	stateListClass.prototype.getAt = function(_index){
+		var name;
+		if( (name = this.indexToName[_index]) == undefined )
+			throw "noSuchState";
+		return this.dataStructure[name];
+	};
+	/*return state obj*/
+	stateListClass.prototype.getAfter = function(name){
+		var index = getIndex.call(this, name);
+		if(index > this.nextInsert)
+			throw "noStateAfter";
+		return this.dataStructure[ this.indexToName[index+1] ];
+	};
+	/*return state obj*/
+	stateListClass.prototype.getBefore = function(name){
+		var index = getIndex.call(this, name);
+		if(index < 1)
+			throw "noStateBefore";
+		return this.dataStructure[ this.indexToName[index-1] ];
+	};
+	stateListClass.prototype.isEmpty = function(){
+		return this.nextInsert == 0;
+	};
+
+	return stateListClass;
 })();
 
-myApp.helper.classes.stateMachine = function(config){
+myApp.helper.classes.linearStateMachine = function(config){
 	config = config || {};
 	var statesArr, statesIndex, activeState, loop = false;
 
-	this.init = function(_config){
-		if(Array.isArray(_config)){
-			statesArr = _config;
-		} else {
-			statesArr = _config.states || [];
-		}
+	var statesList;
 
-		statesIndex = (function(arr){
-			var result = [];
-			for (var i = 0; i < arr.length; i++) {
-				if(arr[i] instanceof myApp.helper.classes.state)
-				result[ arr[i].name ] = i;
-				// console.log('result @'+arr[i].name+' = '+i);
-			};
-			return result;
-		})(statesArr);
-		activeState = (statesArr && statesArr[0] && statesArr[0].name)? statesArr[0].name: null;
+	this.init = function(_config){
+		if( _config instanceof myApp.helper.classes.stateList ){
+			statesList = _config;
+		} else if(Array.isArray(_config)) {
+			statesList = new myApp.helper.classes.stateList(_config);
+		}
+		
+		activeState = this.reset().getName();
+
 		loop = (config['loop'] !== undefined)? config.loop: loop;
 	}
 
@@ -83,28 +140,23 @@ myApp.helper.classes.stateMachine = function(config){
 		return activeState;
 	};
 
-	function find(keyName){
-		return statesArr[ statesIndex[keyName] ];
-	}
-
 	this.nextStep = function(){
 		var nextStateObj;
+
 		try {
-			// console.log('current step: '+activeState);
-			var index = statesIndex[activeState];
-			nextStateObj = statesArr[ index + 1 ];
-			// console.log('index: ' + index);
+			nextStateObj = statesList.getAfter(activeState);
 		} catch(e) {
-			nextStateObj = (loop)? {name: statesArr[0].name}: {name: null};
+			nextStateObj = (loop)? this.reset(): new myApp.helper.classes.state();
 		}
-		activeState = (nextStateObj && nextStateObj['name'] !== undefined)? nextStateObj.name: null;
-		//var current = find($scope.wizStep);
-		//$scope.wizStep = statesArr[current+1];
+
+		activeState = nextStateObj.getName();
 	}
 
 	this.reset = function(){
-		wizStep = statesArr[0];
-		WizardDS.reset(true);
+		var stateObj = statesList.getAt(0);
+		activeState = stateObj.getName();
+
+		return stateObj;
 	}
 
 	this.init(config);
--- test/unit/helperSpec.js
+++ test/unit/helperSpec.js
@@ -3,17 +3,75 @@
 describe('Helpers', function(){
 
 	describe('stateObject', function(){
-		var myState;
+		var myState, rawState = {
+			name: 'someState',
+			blah: { dummy: 'data'}
+		};
 		beforeEach(function(){
-			myState = new myApp.helper.classes.state();
+			myState = new myApp.helper.classes.state(rawState);
 		});
 		it('has name', function(){
-			expect(myState.getName()).not.toBe(undefined);
+			expect(myState.getName).toBeDefined();
+			expect(myState.getName()).not.toBeUndefined();
 			expect(myState.getName().length).toBeGreaterThan(0);
+		});
+		it('can get data', function(){
+			expect(myState.getData).toBeDefined();
+			expect(myState.getData()).toBeDefined();
+			expect(myState.getData().blah.dummy).toMatch('data');
+		})
+		it('returns original name', function(){
+			expect(myState.getName()).toMatch('someState');
 		});
 	});
 
-	describe('stateMachine', function(){
+	describe('stateList', function(){
+		var SL1, SL2, rawList = [
+			{name: 'first', data: {blah: 'someFirstData'}},
+			{name: 'second', data: {blah: 'someSecondData'}},
+			{name: 'third', data: {blah: 'someThirdData'}},
+			{name: 'fourth', data: {blah: 'someFourthData'}}
+		], rawListB = [
+			{name: 'SB', data: {blah: 'fhqwhgads'}},
+			{name: 'HR', data: {blah: 'lngpnts'}}
+		];
+
+		describe('originally empty list', function(){
+			beforeEach(function(){
+				SL1 = new myApp.helper.classes.stateList();
+				SL2 = new myApp.helper.classes.stateList();
+			});
+
+			it('is originally empty', function(){
+				expect(SL1.isEmpty()).toBe(true);
+			});
+		});
+
+		describe('initilized via contructor', function(){
+			beforeEach(function(){
+				SL1 = new myApp.helper.classes.stateList(rawList);
+				SL2 = new myApp.helper.classes.stateList(rawListB);
+			});
+
+			it('is not empty', function(){
+				expect(SL1.isEmpty()).toBe(false);
+			});
+
+			it('can retrieve contained state', function(){
+				var secondState = SL1.get('second');
+				expect(secondState.getName()).toMatch('second');
+				expect(secondState instanceof myApp.helper.classes.state).toBe(true);
+				expect(JSON.stringify(secondState.getData())).toMatch(JSON.stringify(rawList[1]));
+			});
+
+			it('can get state via index', function(){
+				var thirdState = SL1.getAt(2);
+				expect(thirdState.getName()).toMatch('third');
+			});
+		});
+	});
+
+	describe('linearStateMachine', function(){
 		var SM,
 			states = [
 				{ name: 'first',
@@ -24,7 +82,7 @@ describe('Helpers', function(){
 					partial: 'path/to/file.html' }
 			];
 		beforeEach(function(){
-			SM = new myApp.helper.classes.stateMachine(states);
+			SM = new myApp.helper.classes.linearStateMachine(states);
 		});
 
 		it('can advance state', function(){
